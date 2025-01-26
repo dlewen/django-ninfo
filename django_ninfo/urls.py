8b1da988b8eb4149e5a00e65c8b78b175154019b
Daniel Mollberg <dlewen@yta52.net>
2025-01-26 02:41:47 +0100

    change url() to re_path()

--- django_ninfo/urls.py
+++ django_ninfo/urls.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.decorators import login_required
 from django.views.generic import TemplateView
-from django.urls import url, include
+from django.urls import re_path, include
 from django_ninfo import views
 
 from rest_framework import routers
@@ -10,9 +10,9 @@ router.register(r'plugins', views.PluginViewSet, base_name="plugins")
 
 app_name = 'django-ninfo'
 urlpatterns = [
-    url(r'api/plugins/(?P<plugin>\w*)/(?P<format>[a-z]+)/(?P<arg>.*)$', views.PluginResult.as_view()),
-    url(r'api/plugins/(?P<plugin>\w*)/(?P<arg>.*)$', views.PluginResult.as_view()),
-    url(r'api/extract', views.Extract.as_view()),
-    url(r'api/', include(router.urls)),
-    url(r'^$', login_required(TemplateView.as_view(template_name='ninfo/index.html')), name='index'),
+    re_path(r'api/plugins/(?P<plugin>\w*)/(?P<format>[a-z]+)/(?P<arg>.*)$', views.PluginResult.as_view()),
+    re_path(r'api/plugins/(?P<plugin>\w*)/(?P<arg>.*)$', views.PluginResult.as_view()),
+    re_path(r'api/extract', views.Extract.as_view()),
+    re_path(r'api/', include(router.urls)),
+    re_path(r'^$', login_required(TemplateView.as_view(template_name='ninfo/index.html')), name='index'),
 ]
